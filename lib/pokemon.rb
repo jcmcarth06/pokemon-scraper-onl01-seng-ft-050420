27a169f5b318e89d732ddca7e2d4a159278e0f03
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-04 22:35:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id, name, type, db)
-    @id = id 
-    @name = name 
-    @type = type 
+    @id = id
+    @name = name
+    @type = type
     @db = db
   end
 end
